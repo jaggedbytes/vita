77303ab833363d3d956ed91105d9dd0114cac7c1
Bean Jackson <jaggedbytes@protonmail.com>
2022-06-21 13:00:40 -0400

    Update links

--- components/SideNav.js
+++ components/SideNav.js
@@ -9,43 +9,43 @@ export default function SideNav() {
         heightOverride="h-[365px]"
         message="Do my services interest you? 🔮"
         topics=
-          {
-            [
-              "Frontend Development",
-              '"Just Enough" Backend',
-              "Quality Assurance",
-              "Sm/Md Biz IT Consultancy",
-              "Project Management",
-              "Yoga",
-              "Choreo K-Pop Dancing",
-              "Tarot Readings"
-            ]
-          }
-        isGrid={ false }
-        hasLinks={ false }
+        {
+          [
+            "Frontend Development",
+            '"Just Enough" Backend',
+            "Quality Assurance",
+            "Sm/Md Biz IT Consultancy",
+            "Project Management",
+            "Yoga",
+            "Choreo K-Pop Dancing",
+            "Tarot Readings"
+          ]
+        }
+        isGrid={false}
+        hasLinks={false}
       />
 
-      <Drawer 
+      <Drawer
         name="DeFi Passport"
         heightOverride="h-[315px]"
         message="Oh, the blockchains I&apos;ve visited... 🚀"
         topics=
+        {
           {
-            {
-              "Avalanche": "https://www.avax.network/",
-              "Harmony": "https://www.harmony.one/",
-              "Ethereum": "https://ethereum.org/en/",
-              "Solana": "https://solana.com/",
-              "Fantom": "https://fantom.foundation/",
-              "Gnosis": "https://gnosis.io/",
-              "Polygon": "https://polygon.technology/",
-              "NEAR": "https://near.org/",
-              "Cosmos": "https://cosmos.network/",
-              "Terra": "https://www.terra.money/"
-            }
+            "Avalanche": "https://www.avax.network/",
+            "Harmony": "https://www.harmony.one/",
+            "Ethereum": "https://ethereum.org/en/",
+            "Solana": "https://solana.com/",
+            "Fantom": "https://fantom.foundation/",
+            "Gnosis": "https://gnosis.io/",
+            "Polygon": "https://polygon.technology/",
+            "NEAR": "https://near.org/",
+            "Cosmos": "https://cosmos.network/",
+            "Terra": "https://www.terra.money/"
           }
-        isGrid={ true }
-        hasLinks={ true }
+        }
+        isGrid={true}
+        hasLinks={true}
       />
 
       <Drawer
@@ -53,36 +53,37 @@ export default function SideNav() {
         heightOverride="h-[345px]"
         message="Are you seeing these, Anon? 👀"
         topics=
+        {
           {
-            {
-              "DeFi Kingdoms": "https://defikingdoms.com/",
-              "Aavegotchi": "https://aavegotchi.com/",
-              "Parallel": "https://parallel.life/",
-              "The Missing Watcher": "https://www.missingwatcher.com/",
-              "Ember Sword": "https://embersword.com/",
-              "Treeverse": "https://www.treeverse.net/",
-              "DystoPunks": "https://dystopunksv2.com/"
-            }
+            "DeFi Kingdoms": "https://defikingdoms.com/",
+            "Aavegotchi": "https://aavegotchi.com/",
+            "Parallel": "https://parallel.life/",
+            "The Missing Frontier": "https://missingfrontier.com/",
+            "Ember Sword": "https://embersword.com/",
+            "Ragnarok": "https://ragnarok.xyz/",
+            "DystoPunks": "https://dystopunks.net/"
           }
-        isGrid={ false }
-        hasLinks={ true }
+        }
+        isGrid={false}
+        hasLinks={true}
       />
 
       <Container
-        name="Contact"
-        heightOverride="h-[205px]"
+        name="Links"
+        heightOverride="h-[235px]"
         message=""
         topics=
+        {
           {
-            {
-              "Email": "mailto:jaggedbytes@protonmail.com",
-              "LinkedIn": "https://www.linkedin.com/in/jacky-giang-93400b20/",
-              "Github": "https://github.com/jaggedbytes",
-              "🤠🐮🚀": "https://y.at/🤠🐮🚀"
-            }
+            "Résumé": "https://jaggedbytes.s3.amazonaws.com/documents/resume-jacky-giang-2022.pdf",
+            "Email": "mailto:jaggedbytes@protonmail.com",
+            "LinkedIn": "https://www.linkedin.com/in/jacky-giang-93400b20/",
+            "Github": "https://github.com/jaggedbytes",
+            "🤠🐮🚀": "https://y.at/🤠🐮🚀"
           }
-        isGrid={ false }
-        hasLinks={ true }
+        }
+        isGrid={false}
+        hasLinks={true}
       />
     </div>
   )
